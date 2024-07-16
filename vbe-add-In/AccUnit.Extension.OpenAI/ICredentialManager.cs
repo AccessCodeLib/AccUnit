badc92db69981e4bfc1474b27b2960eab1c35c12
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2024-07-16 20:28:00 +0200

    CredentialManger + tests

---
+++ vbe-add-In/AccUnit.Extension.OpenAI/ICredentialManager.cs
@@ -0,0 +1,8 @@
+﻿namespace AccessCodeLib.AccUnit.Extension.OpenAI
+{
+    public interface ICredentialManager
+    {
+        void Save(string target, string username, string secret);
+        string Retrieve(string target);
+    }
+}
