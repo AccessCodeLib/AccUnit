df7c959beef6fd2f2409bf6029fd338b54bab854
Josef Pötzl <josef.poetzl@joposol.at>
2024-07-24 00:32:27 +0200

    + InfoDialog to show Test code generation in progress ...

---
+++ vbe-add-In/AccUnit.VbeAddIn/Info/InfoDialogViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace AccessCodeLib.AccUnit.VbeAddIn
+{
+    public class InfoDialogViewModel : INotifyPropertyChanged
+    {
+        public InfoDialogViewModel()
+        {
+        }
+
+        public InfoDialogViewModel(string infoText)
+        {
+            InfoText = infoText;
+        }   
+
+        private string _infoText;
+        public string InfoText
+        {
+            get { return _infoText; }
+            set
+            {
+                _infoText = value;
+                OnPropertyChanged(nameof(InfoText));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
