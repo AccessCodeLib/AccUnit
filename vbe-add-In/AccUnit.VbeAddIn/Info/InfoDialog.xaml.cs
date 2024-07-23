0b01a64643138e2217a8b720fe4df9656db28d32
Josef Pötzl <josef.poetzl@joposol.at>
2024-07-24 00:32:27 +0200

    + InfoDialog to show Test code generation in progress ...

---
+++ vbe-add-In/AccUnit.VbeAddIn/Info/InfoDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessCodeLib.AccUnit.VbeAddIn
+{
+    /// <summary>
+    /// Interaktionslogik für InfoDialog.xaml
+    /// </summary>
+    public partial class InfoDialog : Window
+    {
+        public InfoDialog(InfoDialogViewModel dataContext)
+        {
+            DataContext = dataContext;
+            InitializeComponent();
+        }
+    }
+}
