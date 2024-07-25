2a691835e68812505c3400d0a5d7b492398a4aa4
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2024-07-25 13:01:05 +0200

    ode cleanup, optimize unit tests

--- vbe-add-In/Tests/AccessCodelib.AccUnit.Extension.OpenAI.Tests/TestCodeBuilderTests.cs
+++ vbe-add-In/Tests/AccessCodelib.AccUnit.Extension.OpenAI.Tests/TestCodeBuilderTests.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AccessCodeLib.AccUnit.Extension.OpenAI;
-
-namespace AccessCodeLib.AccUnit.Extension.OpenAI.Tests
+﻿namespace AccessCodeLib.AccUnit.Extension.OpenAI.Tests
 {
     public class TestCodeBuilderTests
     {
+        private TestCodeBuilder builder;
+
         [SetUp]
         public void Setup()
         {
+            builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
         }
 
         [Test]
         public void BuildTestCode_SimpleTest_DefineTestProcName()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()),new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function GetDate() As Date
     GetDate = Date()
 End Function";
@@ -32,16 +26,14 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.False);
-                Assert.That(testCode.Contains("Public Sub GetDate_CheckIfValueReturnedNot0"), Is.True);
+                Assert.That(testCode, Does.Not.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub GetDate_CheckIfValueReturnedNot0"));
             });
         }
 
         [Test]
         public void BuildTestCode_RowTest_DefineTestProcName()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
     Add = A + B
 End Function";
@@ -54,8 +46,8 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.True);
-                Assert.That(testCode.Contains("Public Sub Add_2Params_CheckResult"), Is.True);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult"));
             });
         }
 
@@ -63,8 +55,6 @@ End Function";
         [Test]
         public void BuildTestCode_RowTest_DefineTestProcNameAndParams()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
     Add = A + B
 End Function";
@@ -78,16 +68,14 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.True);
-                Assert.That(testCode.Contains("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"), Is.True);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"));
             });
         }
 
         [Test]
         public void BuildTestCode_SyncRowTest_DefineTestProcNameAndParams()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
     Add = A + B
 End Function";
@@ -101,8 +89,8 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.True);
-                Assert.That(testCode.Contains("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"), Is.True);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"));
             });
         }
     }
--- vbe-add-In/Tests/FrameworkTests/TestCodeBuilderTests.cs
+++ vbe-add-In/Tests/FrameworkTests/TestCodeBuilderTests.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 
 namespace AccessCodeLib.AccUnit.Extension.OpenAI.Tests
 {
     public class TestCodeBuilderTests
     {
+        private TestCodeBuilder builder;
+
         [SetUp]
         public void Setup()
         {
+            builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
         }
 
         [Test]
         public void BuildTestCode_SimpleTest_DefineTestProcName()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function GetDate() As Date
     GetDate = Date()
 End Function";
@@ -29,16 +29,14 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.False);
-                Assert.That(testCode.Contains("Public Sub GetDate_CheckIfValueReturnedNot0"), Is.True);
+                Assert.That(testCode, Does.Not.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub GetDate_CheckIfValueReturnedNot0"));
             });
         }
 
         [Test]
         public void BuildTestCode_RowTest_DefineTestProcName()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
     Add = A + B
 End Function";
@@ -46,14 +44,13 @@ End Function";
             var testCode = builder.ProcedureToTest(procedureCode, "TestClass")
                                   .TestMethodName("Add_2Params_CheckResult")
                                   .BuildTestMethodCode();
-            
             Console.WriteLine(testCode);
 
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.True);
-                Assert.That(testCode.Contains("Public Sub Add_2Params_CheckResult"), Is.True);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult"));
             });
         }
 
@@ -61,8 +58,6 @@ End Function";
         [Test]
         public void BuildTestCode_RowTest_DefineTestProcNameAndParams()
         {
-            var builder = new TestCodeBuilder(new OpenAiService(new CredentialManager(), new OpenAiRestApiService()), new TestCodePromptBuilder());
-
             var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
     Add = A + B
 End Function";
@@ -76,8 +71,29 @@ End Function";
             Assert.Multiple(() =>
             {
                 Assert.That(testCode, Is.Not.Null);
-                Assert.That(testCode.Contains("'AccUnit:Row"), Is.True);
-                Assert.That(testCode.Contains("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"), Is.True);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"));
+            });
+        }
+
+        [Test]
+        public void BuildTestCode_SyncRowTest_DefineTestProcNameAndParams()
+        {
+            var procedureCode = @"Public Function Add(ByVal A As Integer, ByVal B As Integer) As Integer
+    Add = A + B
+End Function";
+
+            var testCode = builder.ProcedureToTest(procedureCode, "TestClass")
+                                  .TestMethodName("Add_2Params_CheckResult")
+                                  .TestMethodParameters("ByVal intA As Integer, ByVal intB As Integer")
+                                  .BuildTestMethodCode();
+            Console.WriteLine(testCode);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(testCode, Is.Not.Null);
+                Assert.That(testCode, Does.Contain("'AccUnit:Row"));
+                Assert.That(testCode, Does.Contain("Public Sub Add_2Params_CheckResult(ByVal intA As Integer, ByVal intB As Integer, ByVal Expected"));
             });
         }
     }
