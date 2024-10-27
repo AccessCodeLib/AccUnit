53d4363703478c4dc601b46259ed212e0552ed8a
Josef Pötzl <josef.poetzl@joposol.at>
2024-10-27 12:40:37 +0100

    Update Json package

--- access-add-in/_CopyAccUnitLibFilesFromRelease.cmd
+++ access-add-in/_CopyAccUnitLibFilesFromRelease.cmd
@@ -12,5 +12,13 @@ copy .\..\source\AccUnit\bin\Release\AccessCodeLib.*.dll .\lib\
 :: VBE Add-in:
 copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\AccUnit.VbeAddIn.dll* .\lib\
 copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\AccessCodeLib.Common.VbeUserControlHost.dll .\lib\
+copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\AccessCodeLib.AccUnit.Extension.OpenAI.dll .\lib\
+copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\AccessCodeLib.AccUnit.Extension.OpenAI.dll.config .\lib\
+
+copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\Microsoft.Extensions.Configuration.dll .\lib\
+copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\Microsoft.Extensions.Configuration.Abstractions.dll .\lib\
+copy .\..\vbe-add-In\AccUnit.VbeAddIn\bin\Release\Microsoft.Extensions.Configuration.UserSecrets.dll .\lib\
+
+Newtonsoft.Json.dll
 
 timeout 3
