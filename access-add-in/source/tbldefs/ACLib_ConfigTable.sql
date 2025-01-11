a20a342c1a3b8a16e62cfa2e2b483742ff4ee984
Josef Pötzl <josef.poetzl@joposol.at>
2025-01-11 23:54:06 +0100

    Fix: 64 bit bug in AccUnitLoader.accda - WinApiShellTools (Long instead of LongPtr)

--- access-add-in/source/tbldefs/ACLib_ConfigTable.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [ACLib_ConfigTable] (
-  [PropName] VARCHAR (255) CONSTRAINT [PK_ACLib_ConfigTable] PRIMARY KEY UNIQUE NOT NULL,
-  [PropValue] VARCHAR (255),
-  [PropRemarks] LONGTEXT 
-)
